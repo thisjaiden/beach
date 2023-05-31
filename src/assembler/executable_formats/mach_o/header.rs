12026a756b51a6d60dceb462ac6825f3915b85a6
Jaiden Bernard <thisjaiden@gmail.com>
2023-05-31 18:32:51 -0500

    `Header` `write` function

--- src/assembler/executable_formats/mach_o/header.rs
+++ src/assembler/executable_formats/mach_o/header.rs
@@ -1,4 +1,5 @@
 use crate::utils::*;
+use std::convert::TryInto;
 
 pub struct Header {
     /// The magic number for this header. Expected values:
@@ -11,20 +12,21 @@ pub struct Header {
     cpu_type: CPUType,
     // TODO: enum over this for better types
     cpu_subtype: u32,
+    // TODO: enum over this for better types
     file_type: u32,
     number_of_load_commands: u32,
     size_of_load_commands: u32,
+    // TODO: proper flag enum
     flags: u32,
     /// Reserved field only present in 64 bit executables
     _reserved: Option<u32>,
 }
 
-use std::convert::TryInto;
-
 impl Header {
-    /// Generates a [Header] from a type implementing [std::io::Read]. Assumes platform endinanness
+    /// Generates a [Header] from a type implementing [std::io::Read]. Assumes platform endianness
     /// is little.
     pub fn from_le_reader<R: std::io::Read>(reader: &mut R) -> Result<Header, anyhow::Error> {
+        // Read the header values
         let magic_number = u32::from_le_bytes(read_n_bytes(reader, 4)?.try_into().unwrap());
         let cpu_type = CPUType::from_le_reader(reader)?;
         let cpu_subtype = u32::from_le_bytes(read_n_bytes(reader, 4)?.try_into().unwrap());
@@ -32,20 +34,46 @@ impl Header {
         let number_of_load_commands = u32::from_le_bytes(read_n_bytes(reader, 4)?.try_into().unwrap());
         let size_of_load_commands = u32::from_le_bytes(read_n_bytes(reader, 4)?.try_into().unwrap());
         let flags = u32::from_le_bytes(read_n_bytes(reader, 4)?.try_into().unwrap());
+        // This value is only present if this is a 64-bit executable
         let _reserved = if magic_number == 0xFEEDFACF {
             Some(u32::from_le_bytes(read_n_bytes(reader, 4)?.try_into().unwrap()))
         }
         else {
             None
         };
+
+        // Construct the Header type
         Ok(Header {
             magic_number, cpu_type, cpu_subtype,
             file_type, number_of_load_commands, size_of_load_commands,
             flags, _reserved
         })
     }
+    pub fn write<W: std::io::Write>(&self, writer: &mut W) -> Result<(), anyhow::Error> {
+        match self.cpu_type.endianness() {
+            Endian::Little => {
+                return self.write_le(writer);
+            }
+            _ => todo!()
+        }
+    }
+    fn write_le<W: std::io::Write>(&self, writer: &mut W) -> Result<(), anyhow::Error> {
+        writer.write_all(&self.magic_number.to_le_bytes())?;
+        writer.write_all(&(self.cpu_type as u32).to_le_bytes())?;
+        writer.write_all(&self.cpu_subtype.to_le_bytes())?;
+        writer.write_all(&self.file_type.to_le_bytes())?;
+        writer.write_all(&self.number_of_load_commands.to_le_bytes())?;
+        writer.write_all(&self.size_of_load_commands.to_le_bytes())?;
+        writer.write_all(&self.flags.to_le_bytes())?;
+        if self._reserved.is_some() {
+            writer.write_all(&[0x00, 0x00, 0x00, 0x00])?;
+        }
+        writer.flush()?;
+        Ok(())
+    }
 }
 
+#[derive(Clone, Copy)]
 pub enum CPUType {
     VAX = 0x00000001,
     ROMP = 0x00000002,
@@ -90,4 +118,20 @@ impl CPUType {
     pub fn from_le_bytes(bytes: [u8; 4]) -> CPUType {
         CPUType::from_u32(u32::from_le_bytes(bytes))
     }
+    pub fn endianness(&self) -> Endian {
+        match self {
+            CPUType::ARM => Endian::Either,
+            CPUType::ARM64 => Endian::Little,
+            CPUType::X86 => Endian::Little,
+            CPUType::X86ø64 => Endian::Little,
+            // TODO
+            _ => todo!()
+        }
+    }
+}
+
+enum Endian {
+    Big,
+    Little,
+    Either
 }
